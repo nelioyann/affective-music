b139cec16fbe52f9da18cd5b1e81d70c535145ac
nelioyann <nanayannick1998@gmail.com>
2020-06-07 11:59:12 +0200

    _

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,45 @@
 const feedback = (message) => {
-	let feedback = document.querySelector(".feedbackMessage");
+	var feedback = document.querySelector(".feedbackMessage");
 	feedback.innerHTML += `<p>${message}</p>`;
 };
-// let uuid = "00002a37-0000-1000-8000-00805f9b34fb";
+// var uuid = "00002a37-0000-1000-8000-00805f9b34fb";
 const handleChange = (event) => {
-	let value = event.target.value;
-	let a = [];
-	for (let i = 0; i < value.byteLength; i++) {
+	var value = event.target.value;
+	var a = [];
+	for (var i = 0; i < value.byteLength; i++) {
 		a.push("0x" + ("00" + value.getUint8(i).toString(16)).slice(-2));
 	}
 	log("> " + a.join(" "));
 };
-let uuid = "00000009-0000-3512-2118-0009af100700";
-let options = { acceptAllDevices: true, optionalServices: ["heart_rate"] };
+// var uuid = "00000009-0000-3512-2118-0009af100700";
+var options = { acceptAllDevices: true, optionalServices: ["heart_rate"] };
 const trigger = document.querySelector(".main_btn");
 trigger.addEventListener("click", async () => {
 	try {
-		let device = await navigator.bluetooth.requestDevice(options);
+		var device = await navigator.bluetooth.requestDevice(options);
 		feedback(device.name);
 
-		let server = await device.gatt.connect();
+		var server = await device.gatt.connect();
 		console.log(server);
 		feedback("Server connected");
-		let service = await device.gatt.getPrimaryService("heart_rate");
+		var service = await server.getPrimaryService("heart_rate");
 		console.log(service);
 		feedback("Service connected");
 
-		let characteristic = await service.getCharacteristic(0x2a37);
+		var characteristic = await service.getCharacteristic("heart_rate_measurement");
 		console.log(characteristic);
 		feedback("characteristic retrieved");
-
-		let auth = await characteristic.startNotifications();
+		await characteristic.startNotifications();
 		console.log("Notifications have been started.");
+        feedback("Notif activé")
+        // var descriptors = await characteristic.getDescriptors()
+        // console.log(descriptors)
+		// feedback("descriptors retrieved");
 		// characteristic.addEventListener("characteristicvaluechanged", handleChange);
-		// let value = await characteristic.readValue()
+		// var value = await characteristic.readValue()
 		// console.log(value.getInt8(0))
 	} catch (e) {
-		feedback(e);
+        feedback(e)
+		console.log(e);
 	}
 });
