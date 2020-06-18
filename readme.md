3765a578004815d44bdbc23dc27417ae993704bd
nelioyann <nanayannick1998@gmail.com>
2020-06-19 00:32:32 +0200

    layout updated

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Affective Music
+
+Affective Music is an application that synchronize the music your hear to your heart beat.
+
+## Requirements
+
+- Xiaomi Mi Band 4 (other versions haven't been tested yet)
+- Android device with chrome installed
+
+## Does it change anything
+
+I have no idea. But it is possible.
+
+## Usecases
+
+This application could be during a physical activity.
+Intensive workout with high heart rate will lead to high tempo music.
+Moderate workout willbe accompanied with slow tempo music.
+\#4A506D
+\#E2D6D6
+\#3862D3
+\#201324
--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,49 @@
-const feedback = (message) => {
-	var feedback = document.querySelector(".feedbackMessage");
-	feedback.innerHTML += `<li>${message}</li>`;
-};
-// var uuid = "00002a37-0000-1000-8000-00805f9b34fb";
+document.addEventListener("DOMContentLoaded", () => {
 
-const handleChange = (event) => {
-        document.querySelector("audio").play()
+	const hr_value = document.querySelector(".measures__bpm__value");
 
-	let value = event.target.value;
-    // feedback(value)
-	var a = [];
-	for (var i = 0; i < value.byteLength; i++) {
-		a.push("0x" + ("00" + value.getUint8(i).toString(16)).slice(-2));
-	}
-    value = (a.join(" ")).slice(-4)
-	// feedback(typeof value);
-    document.querySelector(".heart").style.animationDuration = `${1300 - parseInt(value) *10}ms`
-    document.querySelector(".hr").innerHTML = `${parseInt(value) }bpm `
-    let playbackspeed = parseInt(value) / 100 + 0.2
-    playbackspeed = playbackspeed.toFixed(1)
-    document.querySelector("audio").playbackRate = playbackspeed
-    feedback(`Play rate speed: ${playbackspeed}`)
-};
-// var uuid = "00000009-0000-3512-2118-0009af100700";
-var options = { acceptAllDevices: true, optionalServices: ["heart_rate"] };
-const trigger = document.querySelector(".connect");
-trigger.addEventListener("click", async () => {
-	try {
-        
-		var device = await navigator.bluetooth.requestDevice(options);
-		feedback(`Connection established to the device`);
 
-		var server = await device.gatt.connect();
-		console.log(server);
-		feedback("Server connected");
-		var service = await server.getPrimaryService("heart_rate");
-		console.log(service);
-		feedback("Heart Rate service connected");
 
-		var characteristic = await service.getCharacteristic("heart_rate_measurement");
-		console.log(characteristic);
-		feedback("Characteristic retrieved");
-		await characteristic.startNotifications();
-		console.log("Notifications have been started.");
-        feedback("Notifications have been started.")
-		characteristic.addEventListener("characteristicvaluechanged", handleChange);
-	} catch (e) {
-        feedback(e)
-		console.log(e);
-	}
+  // var uuid = "00002a37-0000-1000-8000-00805f9b34fb";
+
+  const handleChange = (event) => {
+    // document.querySelector("audio").play();
+
+    let value = event.target.value;
+    var a = [];
+    for (var i = 0; i < value.byteLength; i++) {
+      a.push("0x" + ("00" + value.getUint8(i).toString(16)).slice(-2));
+    }
+    value = a.join(" ").slice(-4);
+    // feedback(typeof value);
+    // document.querySelector(".heart").style.animationDuration = `${1300 - parseInt(value) *10}ms`
+    hr_value.innerHTML = `${parseInt(value)}bpm `;
+    // let playbackspeed = parseInt(value) / 100 + 0.2;
+    // playbackspeed = playbackspeed.toFixed(1);
+    // document.querySelector("audio").playbackRate = playbackspeed;
+    // feedback(`Play rate speed: ${playbackspeed}`);
+  };
+  // var uuid = "00000009-0000-3512-2118-0009af100700";
+  var options = { acceptAllDevices: true, optionalServices: ["heart_rate"] };
+  const trigger = document.querySelector(".measures__heart");
+  trigger.addEventListener("click", async () => {
+    try {
+      var device = await navigator.bluetooth.requestDevice(options);
+      var server = await device.gatt.connect();
+      var service = await server.getPrimaryService("heart_rate");
+      var characteristic = await service.getCharacteristic(
+        "heart_rate_measurement"
+      );
+    //   console.log(characteristic);
+    //   feedback("Characteristic retrieved");
+      await characteristic.startNotifications();
+      characteristic.addEventListener(
+        "characteristicvaluechanged",
+        handleChange
+      );
+    } catch (e) {
+      feedback(e);
+      console.log(e);
+    }
+  });
 });
