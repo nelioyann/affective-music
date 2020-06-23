647d131ab485b544e21fba320700353015400f1e
nelioyann <nanayannick1998@gmail.com>
2020-06-23 11:46:54 +0200

    api deleted, local music used

--- scripts/script.js
+++ scripts/script.js
@@ -32,18 +32,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		this.low_tempo = Math.floor(tempo / 10) * 10;
 		this.high_tempo = Math.ceil(tempo / 10) * 10;
 		this.tracks = [];
-		this.uri = `https://api.happi.dev/v1/music/bpm/playlist/${this.low_tempo}-${this.high_tempo}?&limit=1`;
-		this.headers = new Headers();
-		this.headers.append("Content-Type", "application/json");
-		this.headers.append(
-			"x-happi-key",
-			"ce09b1u4DAwQ5uVNVAYH1jA76cYIQBEHiIPgZuXtP7mDqHbNB8mC5q7B"
-		);
-		this.request = new Request(this.uri, {
-			method: "GET",
-			headers: this.headers,
-			mode: "cors",
-		});
 
 		this.showTracks = () => {
 			tracklist.innerHTML = "";
@@ -63,51 +51,17 @@ document.addEventListener("DOMContentLoaded", () => {
 			console.log(player.tracks)
 			// console.log(player.low_tempo)
 		}
-		// Fetch the data
-		// console.log(player.low_tempo);
-		// console.log(player.high_tempo);
-		// console.log(player.uri);
-		let response = await fetch(player.request);
-		let json = await response.json();
+
 		// Update the player tracks
 		player.tracks = json.tracks;
 		console.log('player tracks', player.tracks)
 		player.showTracks();
-			getTrackSample(player.tracks[0].track);
+		// getTrackSample(player.tracks[0].track);
 
 		// console.log(player.tracks);
 	};
 
-	const getTrackSample = (trackNames) => {
-		// fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
-		// method: "GET"
-		console.log("Getting dem tracks");
-		trackNames.forEach(async (trackName) => {
-			let encodedName = encodeURI(trackName);
-			console.log(trackName, encodedName);
-			let trackUri = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${encodedName}`;
-			// console.log(trackUri);
-			let headers = {
-				"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-				"x-rapidapi-key": "50816b0fbdmsh68dbf1c6445f69ap1ba934jsndbabd5bcae15",
-			};
-			let request = new Request(trackUri, {
-				method: "GET",
-				headers,
-				mode: "cors",
-			});
-			let response = await fetch(request);
-			let json = await response.json();
-			let total = json.total;
-			if (!total) return;
-
-			player.songUrl = json.data[0].preview;
-			
-			// console.log(songUrl);
-
-			// console.log(json);
-		});
-	};
+	
 
 	const handleHeartbeatChange = (event) => {
 		let value = event.target.value;
@@ -117,10 +71,10 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 		value = parseInt(a.join(" ").slice(-4));
 		hr_value.innerHTML = value;
-		switchTracks(player, value);
+		// switchTracks(player, value);
 	};
 
-	trigger.addEventListener("click", async () => {
+	const connect_miband = async () => {
 		console.log("Connection initiated");
 		try {
 			var options = {
@@ -145,19 +99,12 @@ document.addEventListener("DOMContentLoaded", () => {
 		} catch (e) {
 			console.log(e);
 		}
-	});
+	}
+	trigger.addEventListener("click", connect_miband);
 
 	// Testing
 
 	const player = new Player(67);
 
-	// switchTracks(player, 68)
-	// // switchTracks(player, 70)
-	// console.log("Post switch")
-	// const test = () =>{
-	// 	// console.log(test)
-	// 	switchTracks(player, 52)
-	// }
-	// setTimeout(test, 3000)
-	// setTimeout(switchTracks(player, 62), 10000)
+
 });
