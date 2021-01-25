64874ba4bcc0f2478d79915db4d37de557a39b67
nelioyann <nanayannick1998@gmail.com>
2021-01-25 02:38:50 +0100

    added music player ui

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonLabel, IonPage, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from "react";
+import {
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonModal,
+  IonPage,
+  IonRange,
+  IonSegment,
+  IonSegmentButton,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 // import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import "./Tab2.css";
+import { filterOutline, playOutline } from "ionicons/icons";
 
 const Tab2: React.FC = () => {
+
+  const [showModal, setShowModal] = useState(false);
+  const [currentSong, setCurrentSong] = useState("")
+
+
+  const handleMusicSelection = (event: React.MouseEvent<HTMLIonItemElement, MouseEvent>) : void =>{
+    setShowModal(true)
+    let songName = event.currentTarget.getAttribute("data-name") || ""; 
+    setCurrentSong(songName)
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -12,38 +45,89 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
 
+      <IonModal isOpen={showModal} >
+        <IonToolbar>
+          <IonButtons slot="end">
+            <IonButton onClick={() => setShowModal(false)}>
+              <IonIcon  icon={filterOutline}/>
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+
+        <IonCard>
+          <img width="200" src="./assets/logo.png" alt=""/>
+          <IonCardHeader>
+            <IonCardTitle>
+              {currentSong}
+            </IonCardTitle>
+            <IonCardSubtitle>
+              saved on 00.00.0
+            </IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+          <IonItem>
+          <IonRange></IonRange>
+          <IonLabel slot="start">
+            00:00
+          </IonLabel>
+          <IonLabel slot="end">
+            00:00
+          </IonLabel>
+        </IonItem>
+          </IonCardContent>
+
+        </IonCard>
+
+        <IonItem>
+          <IonLabel>
+            Playing Next
+          </IonLabel>
+        </IonItem>
+
+        
+      </IonModal>
+
+        <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Playlist</IonTitle>
           </IonToolbar>
         </IonHeader>
-       <IonCard>
-         <IonCardHeader>
-           <IonCardTitle>Select a synth</IonCardTitle>
-         </IonCardHeader>
-         <IonCardContent>
-        <IonSegment value="am" onIonChange={e => console.log('Segment selected', e.detail.value)}>
-          <IonSegmentButton value="am">
-            <IonLabel>AM</IonLabel>
-          </IonSegmentButton>
-          <IonSegmentButton value="fm">
-            <IonLabel>FM</IonLabel>
-          </IonSegmentButton>
-          <IonSegmentButton value="membrane">
-            <IonLabel>Membrane</IonLabel>
-          </IonSegmentButton>
-        </IonSegment>
-        </IonCardContent>
-        </IonCard>
         <IonCard>
           <IonCardHeader>
-            <IonCardSubtitle>Listen to your past recordings</IonCardSubtitle>
-            <IonCardTitle>Saved entries</IonCardTitle>
+            <IonCardTitle>Select a synth</IonCardTitle>
           </IonCardHeader>
           <IonCardContent>
-            Lorem ipsum dolor sit amet.
+            <IonSegment
+              value="am"
+              onIonChange={(e) =>
+                console.log("Segment selected", e.detail.value)
+              }
+            >
+              <IonSegmentButton value="am">
+                <IonLabel>AM</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton value="fm">
+                <IonLabel>FM</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton value="membrane">
+                <IonLabel>Membrane</IonLabel>
+              </IonSegmentButton>
+            </IonSegment>
           </IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonList>
+            <IonListHeader>Saved entries</IonListHeader>
+            <IonItem data-name="Crescendo" onClick={(e) => handleMusicSelection(e)}>
+              <IonIcon slot="end" icon={playOutline} />
+              <IonLabel>Crescendo</IonLabel>
+            </IonItem>
+            <IonItem data-name="Diminuendo" onClick={(e) => handleMusicSelection(e)}>
+              <IonIcon slot="end" icon={playOutline} />
+              <IonLabel>Diminuendo</IonLabel>
+            </IonItem>
+          </IonList>
         </IonCard>
       </IonContent>
     </IonPage>
