a0e531092f01dadb002da8d10696eaa6a8139ef3
nelioyann <nanayannick1998@gmail.com>
2021-01-27 10:21:58 +0100

    updated tutorial slide

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonSlide, IonSlides, IonTitle, IonToolbar } from '@ionic/react';
-import './Tab3.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonSlide,
+  IonSlides,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Tab3.css";
 
 const Tab3: React.FC = () => {
-
   const slideOpts = {
     initialSlide: 0,
-    speed: 400
+    speed: 400,
   };
   return (
     <IonPage>
@@ -22,30 +29,34 @@ const Tab3: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonSlides mode="ios" pager={true} options={slideOpts}>
-      <IonSlide>
-        <div>
-
-        <img src="./assets/logo.png" alt="Logo"/>
-        <h2>Enable visibility on your Mi App</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit et consequuntur culpa voluptatem reprehenderit sapiente atque maxime aut ex voluptatum.</p>
-        </div>
-      </IonSlide>
-      <IonSlide>
-        <div>
-
-        <img src="./assets/logo.png" alt="Logo"/>
-        <h2>Launch a Freestyle Workout</h2>
-        </div>
-      </IonSlide>
-      <IonSlide>
-      <div>
-
-<img src="./assets/logo.png" alt="Logo"/>
-<h2>Pair your device</h2>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit et consequuntur culpa voluptatem reprehenderit sapiente atque maxime aut ex voluptatum.</p>
-</div>
-      </IonSlide>
-    </IonSlides>
+          <IonSlide>
+            <div>
+              <img src="./assets/logo.png" alt="Logo" />
+              <h3>Enable Mi Band Visibility</h3>
+              <p>
+                To allow pairing with HeartBeats, enable device visibility on
+                the Mi Fit App.{" "}
+              </p>
+            </div>
+          </IonSlide>
+          <IonSlide>
+            <div>
+              <img src="./assets/logo.png" alt="Logo" />
+              <h3>Start a Freestyle Workout Session</h3>
+              <p>
+                In order to increase the heart rate detection rate, try
+                launching a workout exercise session on your Mi Band.
+              </p>
+            </div>
+          </IonSlide>
+          <IonSlide>
+            <div>
+              <img src="./assets/logo.png" alt="Logo" />
+              <h3>Pair your device and record </h3>
+              <p>Pair your device and record a sample of beats. Then listen to your tracks in the resulting playlist.</p>
+            </div>
+          </IonSlide>
+        </IonSlides>
       </IonContent>
     </IonPage>
   );
